79872dc1ae6d36579ce30744dd163cf0a2240d8a
MasahiroSuzuki86 <tobetoeurabemasa@gmail.com>
2024-08-24 09:57:56 +0900

    UseReducerSampleの追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Hello from './components/Hello'
 import Name from './components/Name'
 import Message from './components/Message'
 import Parent from './components/ContainerSample'
 import Page from './components/ContextSample'
 import Counter from './components/HookSample'
+import CounterNeo from './components/UseReducerSample'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,6 +15,7 @@ ReactDOM.render(
     <Parent />
     <Page />
     <Counter initialValue={1} />
+    <CounterNeo initialValue={2} />
   </React.StrictMode>,
   document.getElementById('root')
 )
